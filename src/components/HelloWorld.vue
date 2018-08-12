8b6924de2bd8d3c74d5cfb753a1d30feff42b937
jerry0chu <1052757325@qq.com>
2018-08-12 22:58:21 +0800

    create project structure

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <p>{{responseData}}</p>
+    <button @click="sendGet">sendGet</button>
+    <button @click="sendPost">sendPost</button>
+  </div>
+</template>
+
+<script>
+  import http from '@/api/http'
+
+  export default {
+    name: 'HelloWorld',
+    data()
+    {
+      return {
+        responseData: ''
+      }
+    },
+    methods: {
+      async sendGet()
+      {
+        let params={}
+        const res =await http.get('/hello', params)
+        if (res.data)
+        {
+          alert('请求成功'+res.data)
+          this.responseData=res.data
+        }
+      },
+      async sendPost()
+      {
+          let data={
+            username:"jerry",
+            password:"123"
+          }
+        const res =await http.post('/post', data)
+        if (res.data)
+        {
+          alert('请求成功'+res.data)
+          this.responseData=res.data
+        }
+      },
+    }
+  }
+</script>
